0188d9399459bad1e9414e3ac235481ff119eb9a
samithiwat <samithiwat2544@gmail.com>
2022-05-08 18:49:03 +0700

    Create response struct

--- src/model/common.model.go
+++ src/model/common.model.go
@@ -4,3 +4,8 @@ type PaginationQueryParams struct {
 	Limit int64 `query:"limit"`
 	Page  int64 `query:"page"`
 }
+
+type ResponseErr struct {
+	StatusCode int    `json:"status_code"`
+	Message    string `json:"message"`
+}
