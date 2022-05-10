cea8f427bb3906ba39a15c28e9b7ecdcba1d33d9
samithiwat <samithiwat2544@gmail.com>
2022-05-10 08:58:32 +0700

    Move common model to common dto

---
+++ src/dto/common.dto.go
@@ -0,0 +1,22 @@
+package dto
+
+import "github.com/go-playground/validator/v10"
+
+var validate = validator.New()
+
+type PaginationQueryParams struct {
+	Limit int64 `query:"limit"`
+	Page  int64 `query:"page"`
+}
+
+type ResponseErr struct {
+	StatusCode int         `json:"status_code"`
+	Message    []string    `json:"message"`
+	Data       interface{} `json:"data"`
+}
+
+type BadReqErrResponse struct {
+	FailedField string `json:"failed_field"`
+	Tag         string `json:"tag"`
+	Value       string `json:"value"`
+}
