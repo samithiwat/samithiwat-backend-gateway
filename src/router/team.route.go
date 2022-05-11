20dd82feba48cfb054d6a20373a5f61baf4441e9
samithiwat <samithiwat2544@gmail.com>
2022-05-11 15:33:09 +0700

    Fix error

--- src/router/team.route.go
+++ src/router/team.route.go
@@ -2,31 +2,31 @@ package router
 
 import (
 	"github.com/gofiber/fiber/v2"
-	"github.com/samithiwat/samithiwat-backend-gateway/src/service"
+	"github.com/samithiwat/samithiwat-backend-gateway/src/handler"
 )
 
-func (r *FiberRouter) GetTeam(path string, handler func(ctx service.TeamContext)) {
+func (r *FiberRouter) GetTeam(path string, handler func(handler.TeamContext)) {
 	r.App.Get(path, func(c *fiber.Ctx) error {
 		handler(NewFiberCtx(c))
 		return nil
 	})
 }
 
-func (r *FiberRouter) CreateTeam(path string, handler func(service.TeamContext)) {
+func (r *FiberRouter) CreateTeam(path string, handler func(handler.TeamContext)) {
 	r.App.Post(path, func(c *fiber.Ctx) error {
 		handler(NewFiberCtx(c))
 		return nil
 	})
 }
 
-func (r *FiberRouter) PatchTeam(path string, handler func(service.TeamContext)) {
+func (r *FiberRouter) PatchTeam(path string, handler func(handler.TeamContext)) {
 	r.App.Patch(path, func(c *fiber.Ctx) error {
 		handler(NewFiberCtx(c))
 		return nil
 	})
 }
 
-func (r *FiberRouter) DeleteTeam(path string, handler func(service.TeamContext)) {
+func (r *FiberRouter) DeleteTeam(path string, handler func(handler.TeamContext)) {
 	r.App.Delete(path, func(c *fiber.Ctx) error {
 		handler(NewFiberCtx(c))
 		return nil
