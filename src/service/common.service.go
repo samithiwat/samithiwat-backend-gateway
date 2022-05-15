f8065be22d5d7912875b7eb66e4cb4c87328c9d2
samithiwat <samithiwat2544@gmail.com>
2022-05-15 22:46:55 +0700

    Fix formatErr func

--- src/service/common.service.go
+++ src/service/common.service.go
@@ -3,9 +3,12 @@ package service
 import "fmt"
 
 func FormatErr(errors []string) string {
-	result := errors[0]
-	for i := 1; i < len(errors); i++ {
-		result = fmt.Sprintf("%v, %v", result, errors[i])
+	result := ""
+	if len(errors) > 0 {
+		result = errors[0]
+		for i := 1; i < len(errors); i++ {
+			result = fmt.Sprintf("%v, %v", result, errors[i])
+		}
 	}
 	return result
 }
