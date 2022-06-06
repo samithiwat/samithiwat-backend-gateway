69a50998bd1ffe31387208bee459801c10547c79
samithiwat <samithiwat2544@gmail.com>
2022-06-07 00:08:12 +0700

    Update dto

--- src/dto/organization.dto.go
+++ src/dto/organization.dto.go
@@ -2,5 +2,6 @@ package dto
 
 type OrganizationDto struct {
 	Name        string `json:"name" validate:"required"`
+	Email       string `json:"email" validate:"required"`
 	Description string `json:"description"`
 }
